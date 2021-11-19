26929d4b91ba26526f58bbf484c95013aab7e9df
KarineMarsi <karine.marsi@gmail.com>
2021-11-19 18:12:58 -0300

    Exercicio Função realizado

---
+++ modulo1/funcoes/template_javascript/index.js
@@ -0,0 +1,77 @@
+//Exercicio de interpretação
+//1
+//a.Não iria funcionar 
+//b. 5
+
+//EXERCICIO DE FIXAÇÃO 
+
+// 1.A
+ 
+//  function mensagem() {
+
+// console.log("Eu sou a Karine, tenho 36 nos, moro em São Paulo e sou estudante.")
+
+// }
+//  mensagem()
+// 1.B
+
+// function mensagem (nome,idade,cidade,profissão) {
+
+// console.log(`Eu sou a ${nome}, tenho ${idade}, moro em ${cidade} e sou ${profissão}`)
+
+// }
+
+// 	mensagem("Karine",36, "São Paulo","estudante.")
+
+
+
+
+//2
+// a.
+// function soma(num1 , num2) {
+
+// return num1 + num2
+
+// }
+
+// console.log(soma(18 , 36))
+ //b 
+
+ // function comparar(numero1,numero2) {
+ 	 
+ // 	 return numero1 === numero2
+ // }
+
+ // console.log(comparar( 36,24 ))
+//c 
+// function retornePar(numero3) {
+
+// 	return numero3 % 2 === 0
+// }
+
+// 	console.log(retornePar(36))
+
+//d
+//  function frase (mensagem) {
+//  	// body..
+
+
+// console.log(frase(mensagem.toUppercase().length))
+//  }
+
+// frase("Com grandes poderes, vem grandes responsabilidades")
+
+// Eu tenho certeza que quase acertei essa questão. :-(
+	// body...
+
+3. 
+
+const operaçoes = (numero1,numero2) => {
+	console.log (`Soma:${numero1 + numero2}`)
+	console.log (`Subtração:${numero1 - numero2}`)
+	console.log (`Multiplicação:${numero1 * numero2}`)
+	console.log (`Divisão:${numero1 / numero2}`)
+}
+
+
+operaçoes (36,2)
