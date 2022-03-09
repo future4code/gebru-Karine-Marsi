58e78bfaa867294161c3821929904419504cb0c1
Karine <Karine.marsi@gmail.com>
2022-03-08 22:58:00 -0300

    Exercicio quase completo

---
+++ modulo3/apis-labenusers/src/components/Header/style.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const StyledHeader = styled.header`
+  font-family: Roboto;
+  color: #800000;
+  width: 100%;
+  display:flex;
+  align-items: center;
+  justify-content: center;
+`;
---
+++ modulo3/apis-labenusers/src/pages/home/index.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import Input from "../../components/Input";
+import { Layout } from "./style";
+
+class Home extends React.Component {
+  render() {
+    return (
+      <Layout>
+        <Input />
+      </Layout>
+    );
+  }
+}
+
+export default Home;
