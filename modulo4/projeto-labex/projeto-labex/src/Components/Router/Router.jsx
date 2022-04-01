bfc3f5bbc172063cc471b0b6cd788691f7bc805a
Karine <Karine.marsi@gmail.com>
2022-04-01 12:43:47 -0300

    Atualização de commite

--- modulo4/projeto-labex/projeto-labex/src/Components/Router/Router.jsx
+++ modulo4/projeto-labex/projeto-labex/src/Components/Router/Router.jsx
@@ -1,26 +1,25 @@
 import React from "react";
-import LoginPage from "./LoginPage";
-import ListTripsPage from "./ListTripsPage";
-import TripDetailsPage from "./TripDetailsPage";
-import AdminHomePage from "./AdminHomePage";
-import ApplicationFormPage from "./ApplicationFormPage";
-import CreateTrip from "./CreateTrip";
-import HomePage from "./HomePage";
-import { BrowserRouter, Routs, Route } from "react-router-dom";
+import LoginPage from "../Pages/LoginPage";
+import ListTripsPage from "../Pages/ListTripsPage";
+import TripDetailsPage from "..Pages/TripDetails";
+import AdminHomePage from "../Pages/AdminHomePage";
+import ApplicationFormPage from "../Pages/ApplicationFormPage";
+import CreateTrip from "../Pages/CreateTrip";
+import HomePage from "../Pages/HomePage";
+import { BrowserRouter,Routs, Route } from "react-router-dom";
 
-export const Router = () =>{
- return(
-
-<BrowserRouter>
+export const Router = () => {
+  return (
+    <BrowserRouter>
       <Routs>
-        <Route index element={<HomePage />}/>
-        <Route path = "trips/list" element={< ListTripsPage/>}/>
-        <Route path = "trips/application" element= {<ApplicationFormPage/>}/>
-        <Route path = "login " element={<LoginPage/>}/>
-        <Route path = "admin/trips/list" element={<AdminHomePage/>}/>
-        <Route path = "admin/trips/create" element={<CreateTrip/>}/>
-        <Route path = "admin/trips/:id " element = {<TripDetailsPage/>}/>
+        <Route index element={<HomePage />} />
+        <Route path="trips/list" element={<ListTripsPage />} />
+        <Route path="trips/application" element={<ApplicationFormPage />} />
+        <Route path="login " element={<LoginPage />} />
+        <Route path="admin/trips/list" element={<AdminHomePage />} />
+        <Route path="admin/trips/create" element={<CreateTrip />} />
+        <Route path="admin/trips/:id " element={<TripDetailsPage />} />
       </Routs>
-</BrowserRouter>
-)
-}
+    </BrowserRouter>
+  );
+};
